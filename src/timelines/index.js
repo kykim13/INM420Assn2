c056cde4c32677db90d1e5909d8663d7de90de5e
kyu young kim <kykim13@myseneca.ca>
2020-08-09 16:49:56 -0400

    gsap changed

--- src/timelines/index.js
+++ src/timelines/index.js
@@ -2,11 +2,11 @@ import gsap from 'gsap';
 
 export const play = (pathname, node, appears) => {
   console.log('play', pathname, node, appears);
-  gsap.set(node, { x: '100%', opacity: 0 });
-  gsap.to(node, { duration: 2, x: 0, opacity: 1, ease: 'power2.out' });
+  gsap.set(node, { x: 0, y:0, scale: 1, opacity: 1 });
+  gsap.from(node, { delay: 0.25 ,duration: 0.5,  y: 300, scale: 0.5, opacity: 0, ease: 'poser4.out' });
 }
 
 export const exit = (node) => {
   console.log('exit', node);
-  gsap.to(node, { duration: 0.5, opacity: 0, ease: 'power2.in' });
+  gsap.to(node, { duration: 0.5, x:-300, y:-300, scale:0.5, opacity: 0, ease: 'power2.in' });
 }
